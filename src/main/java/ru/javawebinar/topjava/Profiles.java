5a5458c0bb66ef246dffb9f01c13ccb2e6f029ac
Lissenok88 <sony_2004@mail.ru>
2022-06-30 16:05:19 +0700

    5_8_spring_cache

--- src/main/java/ru/javawebinar/topjava/Profiles.java
+++ src/main/java/ru/javawebinar/topjava/Profiles.java
@@ -14,7 +14,6 @@ public class Profiles {
             POSTGRES_DB = "postgres",
             HSQL_DB = "hsqldb";
 
-    public static final String ACTIVE_DB = POSTGRES_DB;
     //  Get DB profile depending of DB driver in classpath
     public static String getActiveDbProfile() {
         if (ClassUtils.isPresent("org.postgresql.Driver", null)) {
